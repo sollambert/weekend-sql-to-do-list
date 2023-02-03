2b801d9a3b63ddec410717ac67577725f50825b4
Solomon Lambert <sol.lambert@gmail.com>
2023-02-02 21:19:55 -0600

    add key listener to allow inputting task with enter key, clear inputs when submitting

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,42 +4,22 @@ $(() => {
 
 function onReady() {
         $(document).on('click', '#task-btn', submitTask);
+        $(document).on('keypress', 'input', (e) => {
+                if (e.which == 13) {
+                        submitTask();
+                }
+        })
         getTasks();
 }
 
 function getTasks() {
         slay.get('/tasks')
-        .then((response) => {
-                console.log(response);
-                let appendStr = ''
-                for (let task of response) {
-                        let complete = task.complete;
-                        if (complete) {
-                                appendStr += `
-                                <span>
-                                <h3>
-                                  ${task.taskname}
-                                </h3>
-                                <p>
-                                  ${task.taskdesc}
-                                </p>
-                                <input class="incomplete-btn" type="button" value="🚫">
-                                <p>Completed: ${task.timecomplete}</p>
-                                <input class="delete-btn" type="button" value="❌">
-                                </span>`;
-                        } else {
-                                appendStr += `
-                                <span>
-                                <h3>
-                                  ${task.taskname}
-                                </h3>
-                                <p>
-                                  ${task.taskdesc}
-                                </p>
-                                <input class="complete-btn" type="button" value="✅">
-                                <input class="delete-btn" type="button" value="❌">
-                                </span>`;
-                                }
+                .then((response) => {
+                        console.log(response);
+                        let appendStr = ''
+                        for (let task of response) {
+                                appendStr += getTaskAppendStr
+                        (task);
                         }
                         render('#task-div', appendStr);
                 })
@@ -49,21 +29,55 @@ function getTasks() {
                 });
 }
 
+function getTaskAppendStr(task) {
+        let appendStr = ''
+        let complete = task.complete;
+        if (complete) {
+                appendStr += `
+                <span>
+                <h3>
+                  ${task.taskname}
+                </h3>
+                <p>
+                  ${task.taskdesc}
+                </p>
+                <input class="incomplete-btn" type="button" value="🚫">
+                <p>Completed: ${task.timecomplete}</p>
+                <input class="delete-btn" type="button" value="❌">
+                </span>`;
+        } else {
+                appendStr += `
+                <span>
+                <h3>
+                  ${task.taskname}
+                </h3>
+                <p>
+                  ${task.taskdesc}
+                </p>
+                <input class="complete-btn" type="button" value="✅">
+                <input class="delete-btn" type="button" value="❌">
+                </span>`;
+        }
+        return appendStr;
+}
+
 function submitTask() {
         let name = $('#task-name').val();
         let desc = $('#task-desc').val();
-
+        $('#task-name').val('');
+        $('#task-desc').val('');
+        
         let payload = { name, desc };
 
         slay.post('/tasks', payload)
-                .then((response) => {
-                        console.log(response);
-                        getTasks();
-                })
-                .catch((err) => {
-                        console.log(err);
-                        $('body').prepend("Oops we made a fucky wucky...", err);
-                });
+        .then((response) => {
+                console.log(response);
+                getTasks();
+        })
+        .catch((err) => {
+                console.log(err);
+                $('body').prepend("Oops we made a fucky wucky...", err);
+        });
 }
 
 function render(id, string, className, remove) {
